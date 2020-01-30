5253f5f7850c6cec021affc2956c74a8cf80cbf9
Yan Lin <lambertlin@outlook.com>
2020-01-29 23:40:06 -0500

    basic config

---
+++ smart-time-sheet/src/main/java/com/bfs/smarttimesheet/SmartTimeSheetApplication.java
@@ -0,0 +1,12 @@
+package com.bfs.smarttimesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTimeSheetApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SmartTimeSheetApplication.class, args);
+  }
+}
