fa3fe234d673aa98af9e2082cade3e3974df3525
Yan Lin <lambertlin@outlook.com>
2020-01-30 15:08:38 -0500

    getAllSummaries() controller

--- smart-time-sheet/src/main/java/com/bfs/smarttimesheet/controller/WeeklySummaryController.java
+++ smart-time-sheet/src/main/java/com/bfs/smarttimesheet/controller/WeeklySummaryController.java
@@ -1,25 +1,38 @@
 package com.bfs.smarttimesheet.controller;
 
-
 import com.bfs.smarttimesheet.domain.WeeklySummary;
 import com.bfs.smarttimesheet.responseDomain.YearlyVacation;
+import com.bfs.smarttimesheet.service.UserService;
 import com.bfs.smarttimesheet.service.WeeklySummaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/summary")
 public class WeeklySummaryController {
-    private WeeklySummaryService weeklySummaryService;
+  private WeeklySummaryService weeklySummaryService;
+  private UserService userService;
 
-    @Autowired
-    public void setWeeklySummaryService(WeeklySummaryService weeklySummaryService){
-        this.weeklySummaryService = weeklySummaryService;
-    }
+  @Autowired
+  public void setUserService(UserService userService) {
+    this.userService = userService;
+  }
 
+  @Autowired
+  public void setWeeklySummaryService(WeeklySummaryService weeklySummaryService) {
+    this.weeklySummaryService = weeklySummaryService;
+  }
 
-    @GetMapping("/vacationLeft")
-    public YearlyVacation getMessage(@RequestBody WeeklySummary ws) {
-        return this.weeklySummaryService.vacationLeft(ws.getYear());
-    }
+  @PostMapping("/vacationLeft")
+  public YearlyVacation getMessage(@RequestBody WeeklySummary ws) {
+    return this.weeklySummaryService.vacationLeft(ws.getYear());
+  }
+
+  @GetMapping("/all")
+  public List<WeeklySummary> getAllSummaries() {
+    // hardcode username, needs to replace
+    return userService.getAllSummaries("James");
+  }
 }
