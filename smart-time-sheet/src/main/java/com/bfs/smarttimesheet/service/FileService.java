07d89ac1ba750b17a518d618b77b8abadc50ddd6
James <cuibo43@gmail.com>
2020-02-03 16:04:19 -0500

    new

--- smart-time-sheet/src/main/java/com/bfs/smarttimesheet/service/FileService.java
+++ smart-time-sheet/src/main/java/com/bfs/smarttimesheet/service/FileService.java
@@ -1,15 +1,12 @@
 package com.bfs.smarttimesheet.service;
 
-import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public interface FileService {
     void storeFile(MultipartFile file) throws IOException;
     void storePic(MultipartFile file) throws IOException;
 
-//    Resource getFileSystem(String filename, HttpServletResponse response);
 }
 
