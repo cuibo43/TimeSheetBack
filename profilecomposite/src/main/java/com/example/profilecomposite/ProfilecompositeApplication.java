07f72260cab406eafe5c440583f3d73b6b3cbfaa
James <cuibo43@gmail.com>
2020-02-03 22:19:38 -0500

    profilecomposite

---
+++ profilecomposite/src/main/java/com/example/profilecomposite/ProfilecompositeApplication.java
@@ -0,0 +1,17 @@
+package com.example.profilecomposite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class ProfilecompositeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilecompositeApplication.class, args);
+    }
+
+}
---
+++ profilecomposite/src/main/java/com/example/profilecomposite/domain/User.java
@@ -0,0 +1,40 @@
+package com.example.profilecomposite.domain;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class User implements Serializable {
+    private String username;
+
+    private String phoneNumber;
+
+    private String email;
+
+    private String address;
+
+    private String contact1FirstName;
+
+    private String contact1LastName;
+
+    private String contact1PhoneNumber;
+
+    private String contact2FirstName;
+
+    private String contact2LastName;
+
+    private String contact2PhoneNumber;
+
+    private String avatar;
+
+    private String role;
+
+    private String attachment;
+}
